5da3e258c0fd9ac252b6b6c6090462e2fa897a6d
William <William.Jeffery.Knight@protonmail.com>
2022-09-20 08:20:58 -0400

    fix modal to the center

--- client/src/Compponents/Modal.js
+++ client/src/Compponents/Modal.js
@@ -1,39 +1,37 @@
-import { useState, useEffect } from 'react';
-import Axios from 'axios';
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function Modal({ closeModal, title, uncryptedPassword, id, iv }) {
-    const [showPassword, setShowPassword] = useState('');
-
+    const [showPassword, setShowPassword] = useState("");
 
     useEffect(() => {
         Axios.get("http://localhost:3001/showpasswords").then((response) => {
             showPassword(response.data);
         });
-    },);
+    });
 
     const decryptPassword = (encryption, iv) => {
         Axios.post("http://localhost:3001/decryptpassword", {
             password: encryption,
             iv: iv,
         }).then((response) => {
-            setShowPassword(
-                response.data
-            )
-
-
+            setShowPassword(response.data);
         });
-    }
-    decryptPassword(uncryptedPassword, iv)
+    };
+    decryptPassword(uncryptedPassword, iv);
     return (
-        <div className='modal'>
-            <div className='info'>
-            <h1>{title}</h1>
-            <p>Password is <span className='bold'>{showPassword}</span></p>
+        <div className="modal_sytle">
+            <div className="overlay_style">
+                <div className="info">
+                    <h1>{title}</h1>
+                    <p>
+                        Password is <span className="bold">{showPassword}</span>
+                    </p>
+                </div>
+                <button onClick={() => closeModal(false)}>x</button>
             </div>
-            <button onClick={() => closeModal(false)}>x</button>
         </div>
-    )
-
+    );
 }
 
-export default Modal
+export default Modal;
